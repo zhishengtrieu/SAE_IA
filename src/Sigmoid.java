a7d595ff6c47b00c36187a1027c9b64e6059be8f
Hugo COLLIN <hugo.collin1@outlook.fr>
2023-12-11 10:42:15 +0100

    Sigmoid + HyperbolicTangent classes

---
+++ src/Sigmoid.java
@@ -0,0 +1,12 @@
+class Sigmoid implements TransferFunction {
+    @Override
+    public double evaluate(double value) {
+        return 1.0 / (1.0 + Math.exp(-value));
+    }
+
+    @Override
+    public double evaluateDer(double value) {
+        double sigmoid = evaluate(value);
+        return sigmoid * (1 - sigmoid);
+    }
+}
