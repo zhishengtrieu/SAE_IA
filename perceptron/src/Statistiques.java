be754b6be34a92565acf5116a090af4a60284a43
Lim Houn Tchen Aimé <101247250+LimHounTchenAime@users.noreply.github.com>
2023-12-19 13:12:13 +0000

    added knn_desChiffres.md (#9)

--- perceptron/src/Statistiques.java
+++ perceptron/src/Statistiques.java
@@ -9,7 +9,8 @@ import java.io.IOException;
 public class Statistiques {
     public static void main(String[] args) throws IOException {
         AlgoClassification algo = new kNN(10);
-        Donnees testData = new ChargementData("perceptron/data/t10k-images.idx3-ubyte", "perceptron/data/t10k-labels.idx1-ubyte", 100).donnees;
+        int nbImg = 1000;
+        Donnees testData = new ChargementData("perceptron/data/t10k-images.idx3-ubyte", "perceptron/data/t10k-labels.idx1-ubyte", nbImg).donnees;
         int nbCorrect = 0;
         for (Imagette img : testData.getImg()) {
             if (algo.predict(img) == img.getLabel()) {
