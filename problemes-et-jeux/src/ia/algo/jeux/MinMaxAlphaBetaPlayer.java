b1d595366bdc93d45b6f5f9df2a7377ec24c1a90
Zhi-Sheng Trieu <81861610+zhishengtrieu@users.noreply.github.com>
2023-12-19 15:54:24 +0100

    Merge main into algo (#10)
    * AlphaBeta algorithm (Challenge 3) (#8)
    
    * MinMax question 1.1
    
    * AlphaBeta implementation
    
    * Enable to use AlphaBeta
    
    * refactor
    
    * Knn (#7)
    
    * ignore data
    
    * refactor
    
    * @zhishengtrieu KNN-MNIST code
    
    * refactor
    
    * mix with KNN
    
    * rename
    
    * attempts
    
    * poor accuracy
    
    * poor accuracy
    
    * refactor file structure
    
    * Mlp applications report (#5)
    
    * Main Application
    
    * update Main
    
    * report
    
    * 1 update pour XOR sigmoïd
    
    * update AND sigmoïd
    
    * update OR sigmoïd
    
    * update with better choice of layers
    
    * added hyperbolic AND and OR, deleted comments
    
    ---------
    
    Co-authored-by: Hugo COLLIN <hugo.collin1@outlook.fr>
    Co-authored-by: Hugo COLLIN <101000595+Hugo-COLLIN@users.noreply.github.com>
    
    * Problemes et jeux (#6)
    
    * ignore data files
    
    * initial files for games and problems from @zhishengtrieu
    
    * refactor file structure
    
    * fix from professor
    
    ---------
    
    Co-authored-by: Lim Houn Tchen Aimé <101247250+LimHounTchenAime@users.noreply.github.com>
    
    * MinMax question testing and explanation
    
    * try to implement depth and evaluate, but still blocked when player's turn
    
    * use appropriate MinValeur and MaxValeur methods
    
    * refactor
    
    * Part 2 Q1,2,3
    
    * introduce constants
    
    * use Maven to install XChart
    
    * Measure AlphaBeta player decision time
    
    * AlphaBeta benchmark graph
    
    * project name Maven
    
    * move report
    
    ---------
    
    Co-authored-by: Lim Houn Tchen Aimé <101247250+LimHounTchenAime@users.noreply.github.com>
    
    * added knn_desChiffres.md (#9)
    
    ---------
    
    Co-authored-by: Hugo COLLIN <101000595+Hugo-COLLIN@users.noreply.github.com>
    Co-authored-by: Lim Houn Tchen Aimé <101247250+LimHounTchenAime@users.noreply.github.com>

---
+++ problemes-et-jeux/src/ia/algo/jeux/MinMaxAlphaBetaPlayer.java
@@ -0,0 +1,21 @@
+package ia.algo.jeux;
+
+import ia.framework.common.Action;
+import ia.framework.jeux.Game;
+import ia.framework.jeux.GameState;
+import ia.framework.jeux.Player;
+
+public class MinMaxAlphaBetaPlayer extends Player {
+
+    public static final int MAX_DEPTH = 10;
+
+    public MinMaxAlphaBetaPlayer(Game g, boolean p1) {
+        super(g, p1);
+        name = "MinMaxAlphaBeta";
+    }
+
+    @Override
+    public Action getMove(GameState state) {
+        return game.getMinMaxAlphaBetaMove(state, MAX_DEPTH);
+    }
+}
